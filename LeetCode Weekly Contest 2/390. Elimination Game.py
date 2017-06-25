f4671ae76a9efc683399384eecd834c2f000e7b4
linxiaogang45 <zhouiman13@mails.ucas.ac.cn>
2017-06-25 14:19:02 +0800

    Update 390. Elimination Game.py

--- LeetCode Weekly Contest 2/390. Elimination Game.py
+++ LeetCode Weekly Contest 2/390. Elimination Game.py
@@ -1,3 +1,24 @@
+'''There is a list of sorted integers from 1 to n. Starting from left to right, remove the first number and every other number afterward until you reach the end of the list.
+
+Repeat the previous step again, but this time from right to left, remove the right most number and every other number from the remaining numbers.
+
+We keep repeating the steps again, alternating left to right and right to left, until a single number remains.
+
+Find the last number that remains starting with a list of length n.
+
+Example:
+
+Input:
+n = 9
+
+Output:
+6
+'''
+
+
+
+
+
 class Solution(object):
     def lastRemaining(self, n):
         """
