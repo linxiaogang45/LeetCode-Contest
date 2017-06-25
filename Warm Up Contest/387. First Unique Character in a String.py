e6e80117796c4644bcfe495438e1d49a04e4ea0b
linxiaogang45 <zhouiman13@mails.ucas.ac.cn>
2017-06-25 14:37:51 +0800

    Update 387. First Unique Character in a String.py

--- Warm Up Contest/387. First Unique Character in a String.py
+++ Warm Up Contest/387. First Unique Character in a String.py
@@ -1,3 +1,16 @@
+'''
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+'''
+
+
 class Solution(object):
     def firstUniqChar(self, s):
         """
