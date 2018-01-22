7bdb0f17c4cd74e70676d7ce956bf315c15f685f
linxiaogang45 <zhouiman13@mails.ucas.ac.cn>
2018-01-22 09:38:40 +0800

    Update 386.Lexicographical Numbers.py

--- problems/386.Lexicographical Numbers.py
+++ problems/386.Lexicographical Numbers.py
@@ -42,3 +42,22 @@ class Solution(object):
 
 b = Solution()
 print b.lexicalOrder(13)
+
+
+
+
+
+The most elegant python solution from others:
+    
+    class Solution(object):
+    def lexicalOrder(self, n):
+        ans = [1]
+        while len(ans) < n:
+            new = ans[-1] * 10
+            while new > n:
+                new /= 10
+                new += 1
+                while new % 10 == 0:    # deal with case like 199+1=200 when we need to restart from 2.
+                    new /= 10
+            ans.append(new)
+        return ans
